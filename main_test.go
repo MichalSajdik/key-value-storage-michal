4da7efe440c6722b6fd97ad9655f966b8e00eee2
Michal696 <30591067+Michal696@users.noreply.github.com>
2021-09-22 22:55:38 +0200

    add tests

---
+++ main_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	b64 "encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type TestSuiteMain struct {
+	suite.Suite
+}
+
+func (t *TestSuiteMain) SetupSuite() {
+}
+
+func TestMainTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuiteMain))
+}
+
+func (t *TestSuiteMain) TestCreateRequest() {
+
+	req, err := http.NewRequest("POST", "", bytes.NewBuffer([]byte(`{
+		"Key": "key2",
+		"Data": "bGtua1pubQ==",
+		"Timestamp": "2022-11-01T22:08:41Z"
+	}`)))
+	if err != nil {
+		t.FailNow(err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(post)
+
+	// Our handlers satisfy http.IndexHandler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusCreated {
+		t.FailNow("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+}
+
+func (t *TestSuiteMain) TestGetRequest() {
+
+	req, err := http.NewRequest("GET", "", bytes.NewBuffer([]byte(`{
+		"Key": "key2"
+	}`)))
+	if err != nil {
+		t.FailNow(err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(get)
+
+	// Our handlers satisfy http.IndexHandler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.FailNow("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	body, err := ioutil.ReadAll(rr.Body)
+	if err != nil {
+		t.FailNow(err.Error())
+	}
+
+	var rawJsonRequest createRequestRaw
+	err = json.Unmarshal(body, &rawJsonRequest)
+	if err != nil {
+		t.FailNow(err.Error())
+	}
+
+	if rawJsonRequest.Key != "key2" {
+		t.FailNow("Returned wrong key: got %s want %s",
+			rawJsonRequest.Key, "key2")
+	}
+
+	if b64.StdEncoding.EncodeToString(rawJsonRequest.Data) != "bGtua1pubQ==" {
+		t.FailNow("Returned wrong data: got %v want %v",
+			rawJsonRequest.Data, []byte("bGtua1pubQ=="))
+	}
+
+	if rawJsonRequest.Timestamp != "2022-11-01T22:08:41Z" {
+		t.FailNow("Returned wrong timestamp: got %v want %v",
+			rawJsonRequest.Timestamp, "2022-11-01T22:08:41Z")
+	}
+
+}
